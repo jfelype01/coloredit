710b403c347e6b62997402592d812ce701403fb5
jfelype01 <joaofelype015@gmail.com>
2022-08-23 21:26:21 -0300

    consertando bugs do input hsl

--- scripts/hslCodes.js
+++ scripts/hslCodes.js
@@ -8,5 +8,28 @@ hsl.addEventListener('input', function() {
         color[i] = parseInt(color[i])
     }
 
-    hex.value = rgbToHex(rgb.value);
+    hex.value = `#${rgbToHex(rgb.value)}`;
 })
+
+hsl.onchange = () => {
+    let hslValue = hsl.value.split(',');
+
+    if(hslValue.length > 3) {
+        hsl.value = hslValue[0]
+        for (let i = 1; i < 3; i++) {
+            hsl.value += `,${hslValue[i]}`
+        }
+    }
+
+    hslValue = hsl.value.split(',');
+
+    if(hslValue.length === 3) {
+        if(hslValue[1][hslValue[1].length - 1] !== "%") {
+            hslValue[1] += "%"
+        }
+        if(hslValue[2][hslValue[2].length - 1] !== "%") {
+            hslValue[2] += "%"
+        }
+        hsl.value = hslValue.join(', ')
+    } 
+}
